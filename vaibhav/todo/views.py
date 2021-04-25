bff2ad1499688c443b9c4055820806e3634c4651
vaibhavnaik1 <83159092+vaibhavnaik1@users.noreply.github.com>
2021-04-25 14:28:54 +0530

    project

---
+++ vaibhav/todo/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.contrib import messages
+from .forms import TodoForm
+from .models import Todo
+
+
+
+def index(request):
+
+	item_list = Todo.objects.order_by("-date")
+	if request.method == "POST":
+		form = TodoForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('todo')
+	form = TodoForm()
+
+	page = {
+			"forms" : form,
+			"list" : item_list,
+			"title" : "TODO LIST",
+		}
+	return render(request, 'todo/index.html', page)
+
+
+
+def delete_data(request, item_id):
+    if request.method=='POST':
+	    item = Todo.objects.get(id=item_id)
+	    item.delete()
+	    messages.info(request, "item removed !!!")
+	    return redirect('todo')
+
+
+
